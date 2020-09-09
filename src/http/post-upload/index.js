9d4d1f03288be524f777292c608303b21774be6a
Paul Chin Jr <pchinjr@gmail.com>
2020-09-09 09:47:32 -0400

    upload trial

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -1,16 +1,32 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 
-let arc = require('@architect/functions')
-let aws = require('aws-sdk')
+const path = require('path')
 const fs = require('fs')
-
+const aws = require('aws-sdk')
 exports.handler = async function http(req) {
 
   let body = req.body //base64 encoded string
-  let buff = Buffer.from(body, 'utf8') //buffer that converts to string
+  let buff = Buffer.from(body) //buffer that converts to string
   let b64 = buff.toString('base64') // still the wrong one - needs to be decoded again
   let secondBuff = Buffer.from(b64, 'base64')
   let decoded = secondBuff.toString('utf8') //still not decoded
+
+  if (process.env.NODE_ENV === 'testing') {
+    let pathToPublic = path.join(__dirname, '..', '..', '..', 'public', 'image.jpg')
+    fs.writeFileSync(pathToPublic, decoded)
+  }
+  else {
+    // write to s3
+    let s3 = new aws.S3
+    await s3.putObject({
+      ACL: 'public-read',
+      Bucket: process.env.ARC_STATIC_BUCKET,
+      Key: `${process.env.ARC_STATIC_FOLDER}/image.jpg`,
+      Body: decoded,
+      ContentType: `image/jpeg`,
+      CacheControl: 'max-age=315360000',
+    })
+  }
 
   return {
     statusCode: 200,
